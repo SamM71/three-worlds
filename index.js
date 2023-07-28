7c0cbdcbc9141a49d7dc78683492fa1461170035
SamM71 <sammocock@gmail.com>
2023-07-28 17:04:38 +0100

    stop chickenburger autoplay

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const prompt = require(`prompt-sync`)({sigint: true})
 const colours = require(`ansi-colors`)
 
+
+const chickenBurger = require('./world2')
 const avocadoBurger = require(`./world3`)
 
 
@@ -8,7 +10,7 @@ const avocadoBurger = require(`./world3`)
 const title = `\n \n ---You are playing THREE WORLDS by Nathan, Rubina, and Sam--- \n\n`
 const openingText = `You're hungry.\nIt's cold and wet.\nYou look up at the flashing neon sign of the only restaurant for the next 50 miles.
 "No wonder this place is a ghost town" you think to yourself, before opening the door and smelling the odours of freshly cooked patties.
-You look up at the menu and see three choices: \n A. Cheeseburger Royale, \n B. Champion Chicken Burger, \n C. Amazing Avo Burger.`
+You look up at the menu and see three choices: (1) Cheeseburger Royale, (2) Champion Chicken Burger, (3) 3. Amazing Avo Burger.`
 
 function startGame() {
 
@@ -25,17 +27,19 @@ function startGame() {
       break;
     } else if (choice === "2") {
       console.log(`You ask for the chicken burger.`)
-      const chickenBurger = require('./world2')
+      enterWorld()
       chickenBurger()
       break;
     } else if (choice === "3") {
       console.log(`You ask for the veggie option.`)
+      enterWorld()
+      avocadoBurger()
       break; 
     } else {
       console.log('Invalid input, please input only one of the three options above')
     }
   }
-  enterWorld(choice)
+  enterWorld()
 }
 
 function endGame() {
@@ -49,12 +53,11 @@ startGame()
 
 endGame()
 
-function enterWorld(choice) {
+function enterWorld() {
   const opening = `\nYou look down at your burger and notice that it looks nowhere near as good as it did on the sign.
   "Oh well" you say to yourself as you take a bite that changes your life.
   You've never quite eaten anything like this, it's just so perfect.
   You get completely lost in the burger, shutting your eyes to truly appreciate the taste....\n`
 
   console.log(opening)
-  avocadoBurger(choice)
 }
--- world2.js
+++ world2.js
@@ -218,7 +218,7 @@ function chickenBurger () {
     }
   }
   
-chickenBurger()
+//chickenBurger()
 
 
 
