a0920d1d54e0944e50dad69fc999c99118252d37
“rkumari2” <rubinakumari@mail.com>
2023-07-28 14:06:45 +0100

    final commit

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const prompt = require(`prompt-sync`)({sigint: true})
 const colours = require(`ansi-colors`)
 
 
+
 const title = `\n \n ---You are playing THREE WORLDS by Nathan, Rubina, and Sam--- \n\n`
 const openingText = `You're hungry.\nIt's cold and wet.\nYou look up at the flashing neon sign of the only restaurant for the next 50 miles.
 "No wonder this place is a ghost town" you think to yourself, before opening the door and smelling the odours of freshly cooked patties.
-You look up at the menu and see three choices: \n - Cheeseburger Royale, \n - Champion Chicken Burger, \n - Amazing Avo Burger.`
+You look up at the menu and see three choices: \n A. Cheeseburger Royale, \n B. Champion Chicken Burger, \n C. Amazing Avo Burger.`
 
 function startGame() {
 
@@ -17,19 +18,26 @@ function startGame() {
   while (!isValidChoice) {
     const choice = prompt(`Which burger do you choose?: `)
 
-    if (choice == `cheeseburger royale`) {
+    if (choice === "a") {
       console.log(`You ask for the cheeseburger.`)
       break;
-    } else if (choice == `champion chicken burger`) {
+    } else if (choice === "b") {
       console.log(`You ask for the chicken burger.`)
+      const chickenBurger = require('./world2')
+      chickenBurger()
       break;
-    } else if (choice === 'amazing avo burger') {
+    } else if (choice === "c") {
       console.log(`You ask for the veggie option.`)
       break; 
     } else {
-      console.log('invalid input, please input only one of the three options above')
+      console.log('Invalid input, please input only one of the three options above')
     }
   }
 }
 
 startGame()
+
+
+
+
+
